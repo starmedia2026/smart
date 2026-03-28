1d410a0ada5d42d1ba2666d0447a2a2f57176ca0
ستار ميديا للانتاج الاعلامي <star.media2026@gmail.com>
2026-03-28 09:12:08 +0000

    عند فتح في الجوال الموقع Favicon يظهر الايقوانة ممتاز - لاكن عند فتح الم

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         
         {/* Favicons */}
-        <link rel="icon" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
-        <link rel="icon" type="image/png" sizes="16x16" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
+        <link rel="shortcut icon" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
+        <link rel="icon" type="image/jpeg" sizes="32x32" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
+        <link rel="icon" type="image/jpeg" sizes="16x16" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
         <link rel="apple-touch-icon" sizes="180x180" href="https://i.postimg.cc/t41FxxDT/IMG-20260318-WA0005(1).jpg" />
       </head>
       <body className="font-body antialiased bg-background text-foreground selection:bg-primary selection:text-primary-foreground">
